9a29147119501912e55bffe4d2da45cf5746ca5a
sammalviy <sammalviya949@gmail.com>
2024-09-26 16:24:48 +0530

    second commit

---
+++ Sam2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Sam2 {
+
+	public static void main(String ar[]) 
+	{
+		
+		int x=Integer.parseInt(ar[0]);
+		int y=Integer.parseInt(ar[1]);
+		System.out.println(x/y);
+		System.out.println("Sam__664");
+		
+	}
+}
+
+	
+
+// runtime stack mechanism in java
+
+// Exception prapogation
+
