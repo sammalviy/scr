c8de542c50cd1655ef48ac4cb2a203827805a994
sammalviy <sammalviya949@gmail.com>
2024-09-26 09:36:31 +0530

    first commit

---
+++ Sam.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Sam {
+
+	public static void main(String ar[]) {
+		
+		 Scanner sc = new Scanner(System.in);
+
+	     System.out.print("Enter your num: ");
+	     int num = sc.nextInt();
+	     
+	     for(int i=0 ; i<10 ; i++){
+	     
+	     System.out.println(i +" "+ num--);
+		
+	     }
+	}
+
+}
